c27c3615b9128a1a837bbe8d2643f00ec467c31d
yapcheahshen <yapcheahshen@gmail.com>
2024-09-18 21:37:54 +0800

    moving to offtextview

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # guanyin
+
+logo taken from https://vocus.cc/article/6143632afd89780001d7d1af 方晴君
--- build-data.cmd
+++ build-data.cmd
@@ -3,3 +3,7 @@ cmd/c ptk ptk guanyin-bh
 cmd/c ptk ptk guanyin-en
 cmd/c ptk ptk guanyin-pl
 cmd/c ptk ptk guanyin-sk
+cd ..\dhammahall\
+cmd/c ptk ptk
+cd ..\guanyin\
+copy/y ..\dhammahall\dist\dhammahall.ptk dist
