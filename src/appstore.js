4bc2fe45aca9a11b04fa0f33e5d6da8b48a5f741
yapcheahshen <yapcheahshen@gmail.com>
2024-10-01 21:11:44 +0800

    back transclusion ok

--- src/appstore.js
+++ src/appstore.js
@@ -2,7 +2,7 @@ export * from 'accelon23/src/store.js';
 import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
 
 _ACC23.CacheName='v1::guanyin',
-_ACC23.AppVer='24.9.16';
+_ACC23.AppVer='24.10.1';
 _ACC23.AppName='guanyin'
 _ACC23.AppTitle='觀音系列'
 _ACC23.allptks=['guanyin','guanyin-sk','guanyin-en','guanyin-bh','guanyin-pl','dhammahall'];
